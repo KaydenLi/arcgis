daa4373db8860d1f2f660cce30718585e1e6b901
KaydenLi <820807451@qq.com>
2020-05-30 15:08:24 +0800

    引如element

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,10 +1,7 @@
 <template>
   <div>
     <div class="title">ArcGis test</div>
-    <div ref="mapContainer" class="map" id="map"></div>
-    <!-- <div ref="map" class="map">
-      <div ref="button"></div>
-    </div>-->
+    <div ref="mapContainer" class="map"></div>
   </div>
 </template>
 
@@ -21,25 +18,18 @@ export default {
     loadModules(
       [
         "esri/map",
-        "esri/layers/TiledMapServiceLayer",
+        "esri/layers/ArcGISDynamicMapServiceLayer",
         "esri/layers/FeatureLayer",
-        "esri/symbols/SimpleMarkerSymbol",
-        "esri/symbols/SimpleLineSymbol",
-        "esri/renderers/SimpleRenderer",
-        "dojo/_base/Color"
+        "dojo/domReady!"
       ],
-      { url: baseUrl.apiUrl, css: baseUrl.cssUrl }
+      {
+        url: baseUrl.apiUrl,
+        css: baseUrl.cssUrl
+      }
     )
       .then(
-        ([
-          Map,
-          Tiled,
-          FeatureLayer,
-          SimpleMarkerSymbol,
-          SimpleLineSymbol,
-          SimpleRenderer,
-          Color
-        ]) => {
+        ([Map, ArcGISDynamicMapServiceLayer, FeatureLayer]) => {
+       
           // 创建一个底图
           const map = new Map(this.$refs.mapContainer, {
             basemap: "topo",
@@ -47,38 +37,19 @@ export default {
             zoom: 10, // 当前地图缩放等级
             logo: false
           });
-
-          map.on("load", () => {
-            // 创建一个图层
-            const baseLayer = new Tiled(baseUrl.baseMap);
-            // 将图层添加到map中
-            map.addLayer(baseLayer, 1);
-
-            // 创建一个line用做marker的outline
-            const layer2Outline = new SimpleLineSymbol({
-              style: SimpleLineSymbol.STYLE_SOLID,
-              color: new Color([255, 0, 0]),
-              size: 1
-            });
-            // 创建一个marker
-            const layer2Symbol = new SimpleMarkerSymbol({
-              style: SimpleMarkerSymbol.STYLE_SQUARE,
-              size: 10,
-              outline: layer2Outline,
-              color: new Color([0, 255, 0, 0.25])
-            });
-            //创建一个render
-            const layer2Sr = new SimpleRenderer({
-              symbol: layer2Symbol
-            });
-            // 创建新图层
-            const layer = new FeatureLayer(baseUrl.layer2, {
-              title: "layer2"
-              // renderer: layer2Sr
-            });
-            // 将图层添加到map中
-            map.addLayer(layer, 2);
+        //   map.spatialReference = { wkid: 4326 };
+          // 创建图层
+          const baseLayer = new ArcGISDynamicMapServiceLayer(baseUrl.baseMap);
+          console.log(baseLayer);
+          // 将图层添加到map中
+          map.addLayer(baseLayer, 1);
+          // console.log("map", map.getLayers());
+          // // 创建新图层
+          const layer = new FeatureLayer(baseUrl.layer2, {
+            title: "layer2"
           });
+          // 将图层添加到map中
+          map.addLayer(layer, 2);
         }
       )
       .catch(err => {
